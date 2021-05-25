cbc1cc45de9cd0c4371281c0bdffa44edb942b59
Zamyatin Vyacheslav <zamyatinvg@gmail.com>
2021-05-25 11:15:42 +0300

    0.9.2 - Шатура - https://jira.askona.ru/browse/ERP-4640

---
+++ HonestSign/Controllers/ShaturaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HonestSign.Controllers
+{
+    public class ShaturaController : Controller
+    {
+        [HttpGet]
+        [ValidateInput(false)]
+        public JsonResult GetBarcode(string id)
+        {
+            return Json(Models.Shatura.GetBarcode(id), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HonestSign/Models/Shatura.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Configuration;
+using RestSharp;
+using Newtonsoft.Json;
+using NLog;
+
+namespace HonestSign.Models
+{
+    public class Shatura
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
+        static Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+        public class Barcode
+        {
+            public string barcode { get; set; }
+            public string error { get; set; }
+        }
+        public static string GetBarcode(string id)
+        {
+            try
+            {
+                var client = new RestClient($"https://apps.shatura.com/shatura1/bc/getbarcode/token={config.AppSettings.Settings["ShaturaToken"].Value}/ingred={id}");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(request);
+                logger.Info($"Данные по товару Шатуры ({id}) успешно получены.");
+                Barcode barcode = JsonConvert.DeserializeObject<Barcode>(response.Content);
+                if (barcode.error != null)
+                {
+                    logger.Error($"Сервис Шатуры вернул сообщение об ошибке по товару ({id}): {barcode.error}");
+                    return "";
+                }
+                else
+                    return ($"{barcode.barcode}");
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Ошибка получения данных по товару Шатуры ({id})!\n" + ex.Message);
+                return "";
+            }
+        }
+    }
+}
