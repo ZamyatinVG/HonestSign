aaea082f34e931223141a33ff87f6cc9aafb974d
Zamyatin Vyacheslav <zamyatinvg@gmail.com>
2022-05-11 20:35:17 +0300

    0.9.11 - Логгирование

--- HonestSign/Controllers/KMController.cs
+++ HonestSign/Controllers/KMController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Web.Mvc;
+using NLog;
 
 namespace HonestSign.Controllers
 {
     public class KMController : Controller
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         [HttpGet]
         [ValidateInput(false)]
         public JsonResult GetStatus(string km, string filial)
         {
+            logger.Info($"Вызов метода KMController.GetStatus: km = {km}, filial = {filial}");
             return Json(Models.KM.GetStatus(km, false, filial ?? "td"), JsonRequestBehavior.AllowGet);
         }
     }
